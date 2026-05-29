7775991bea625fe89674b2d3ebead79440d316c3
Deivid Silviev Yordanov <deividyordanov@abv.bg>
2026-05-30 00:15:14 +0300

    Added Astronaut class

---
+++ SpaceMission2026/Astronaut.cs
@@ -0,0 +1,16 @@
+﻿namespace SpaceMission2026
+{
+    public class Astronaut
+    {
+        public string? Name { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
+
+        public Astronaut(string name, int row, int col)
+        {
+            Name = name;
+            Row = row;
+            Col = col;
+        }
+    }
+}
