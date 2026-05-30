d54365d8f14bfc2a92a6581d62e599fff5269d30
Deivid Silviev Yordanov <deividyordanov@abv.bg>
2026-05-30 15:44:47 +0300

    Implemented error handling

--- SpaceMission2026/Program.cs
+++ SpaceMission2026/Program.cs
@@ -8,16 +8,43 @@
             int rows = int.Parse(Console.ReadLine());
             Console.Write("Map columns: ");
             int cols = int.Parse(Console.ReadLine());
+
+            if (rows < 2 || rows > 100 || cols < 2 || cols > 100)
+            {
+                Console.WriteLine("Invalid map size. Rows and columns must be between 2 and 100.");
+                return;
+            }
+
             Console.WriteLine("Cosmic map: ");
 
             string[,] matrix = new string[rows, cols];
 
             List<Astronaut> astronauts = new();
 
+            int checkForSpaceStation = 0;
+
+            // Read the cosmic map and identify astronauts
             for (int row = 0; row < rows; row++)
             {
                 string[] input = Console.ReadLine()
                     .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (input.Length != cols)
+                {
+                    Console.WriteLine(
+                        $"Expected {cols} columns but got {input.Length}.");
+                    return;
+                }
+
+                if (input.Any(x =>
+                     x != "0" &&
+                     x != "X" &&
+                     x != "F" &&
+                     !x.StartsWith("S")))
+                {
+                    Console.WriteLine("Invalid map symbol.");
+                    return;
+                }
 
                 for (int col = 0; col < cols; col++)
                 {
@@ -27,14 +54,35 @@
                     {
                         astronauts.Add(new Astronaut(matrix[row, col], row, col));
                     }
+                    
+                    else if (matrix[row, col] == "F")
+                    {
+                        checkForSpaceStation++;
+                    }
                 }
             }
 
+            if (checkForSpaceStation != 1)
+            {
+                Console.WriteLine("The map must contain exactly one space station.");
+                return;
+            }
+
+            if (astronauts.Count == 0 || astronauts.Count > 3)
+            {
+                Console.WriteLine("Invalid number of astronauts. There must be between 1 and 3 astronauts on the map.");
+                return;
+            }
+
+
+
+            // Create a PathFinder instance and find paths for each astronaut
             PathFinder pathFinder = new PathFinder(matrix);
 
             List<MissionResult> results = new();
             List<string> failedAstronauts = new();
 
+            // Find paths for each astronaut and store results
             foreach (Astronaut astronaut in astronauts)
             {
                 MissionResult? result = pathFinder.FindPath(astronaut);
