5877b02dcc68cb752f4bab83122b5544697f1512
Deivid Silviev Yordanov <deividyordanov@abv.bg>
2026-05-30 12:02:32 +0300

    Refactor program.cs

--- SpaceMission2026/Program.cs
+++ SpaceMission2026/Program.cs
@@ -9,105 +9,50 @@
 
             string[,] matrix = new string[rows, cols];
 
-            // all astronauts with their starting point
-            Dictionary<string, (int row, int col)> astronauts = new Dictionary<string, (int row, int col)>();
+            List<Astronaut> astronauts = new();
 
-            //
-            int count = 0;
-
-            // fill the matrix
-            for (int row = 0; row < matrix.GetLength(0); row++)
+            for (int row = 0; row < rows; row++)
             {
-                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-                for (int col = 0; col < matrix.GetLength(1); col++)
+                for (int col = 0; col < cols; col++)
                 {
                     matrix[row, col] = input[col];
 
                     if (matrix[row, col].StartsWith('S'))
                     {
-                        if (astronauts.ContainsKey(matrix[row, col]))
-                        {
-                            astronauts[matrix[row, col]] = (row, col);
-                        }
-                        else
-                        {
-                            astronauts.Add(matrix[row, col], (row, col));
-                        }
+                        astronauts.Add(new Astronaut(matrix[row, col], row, col));
                     }
                 }
             }
 
-            Queue<(int row, int col, int count)> queue = new();
-            int[] dRow = { -1, 1, 0, 0 };
-            int[] dCol = { 0, 0, -1, 1 };
+            PathFinder pathFinder = new PathFinder(matrix);
 
-            int newRow, newCol = 0;
-
-            //Space travel logic
-
-            foreach (var astronaut in astronauts)
+            foreach (Astronaut astronaut in astronauts)
             {
-                (int row, int col)[,] path = new (int row, int col)[matrix.GetLength(0), matrix.GetLength(1)];
-                bool[,] visited = new bool[matrix.GetLength(0), matrix.GetLength(1)];
-                string[,] newMatrix = (string[,] )matrix.Clone();
+                string[,] resultMatrix = pathFinder.FindPath(astronaut);
 
-                int currentRow = astronaut.Value.row;
-                int currentCol = astronaut.Value.col;
-                int startRow = astronaut.Value.row;
-                int startCol = astronaut.Value.col;
-                visited[currentRow, currentCol] = true;
+                Console.WriteLine($"Path for {astronaut.Name}:");
+                PrintMatrix(resultMatrix);
+            }
+        }
 
-                queue.Enqueue((currentRow, currentCol, 0));
-
-                while (queue.Count > 0)
+        private static void PrintMatrix(string[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
                 {
-                    (int row, int col, int count) current = queue.Dequeue();
-                    currentRow = current.row;
-                    currentCol = current.col;
-                    count = current.count;
+                    Console.Write(matrix[row, col]);
 
-                    for (int i = 0; i < 4; i++)
+                    if (col < matrix.GetLength(1) - 1)
                     {
-                        if (currentRow + dRow[i] >= 0 && currentRow + dRow[i] < matrix.GetLength(0) &&
-                            currentCol + dCol[i] >= 0 && currentCol + dCol[i] < matrix.GetLength(1) &&
-                            !visited[currentRow + dRow[i], currentCol + dCol[i]] &&
-                            matrix[currentRow + dRow[i], currentCol + dCol[i]] != "X")
-                        {
-                            if (matrix[currentRow + dRow[i], currentCol + dCol[i]] == "F")
-                            {
-                                newRow = currentRow + dRow[i];
-                                newCol = currentCol + dCol[i];
-                                path[newRow, newCol] = (currentRow, currentCol);
-
-                                int row = newRow;
-                                int col = newCol;
-
-                                while (!(row == startRow && col == startCol))
-                                {
-                                    if (newMatrix[row, col] != "F")
-                                    {
-                                        newMatrix[row, col] = "*";
-                                    }
-
-                                    (int row, int col) previous = path[row, col];
-
-                                    row = previous.row;
-                                    col = previous.col;
-                                }
-                                break;
-                            }
-                            if (matrix[currentRow + dRow[i], currentCol + dCol[i]] == "O")
-                            {
-                                newRow = currentRow + dRow[i];
-                                newCol = currentCol + dCol[i];
-                                visited[newRow, newCol] = true;
-                                path[newRow, newCol] = (currentRow, currentCol);
-                                queue.Enqueue((newRow, newCol, count + 1));
-                            }
-                        }
+                        Console.Write(" ");
                     }
                 }
+
+                Console.WriteLine();
             }
         }
     }
